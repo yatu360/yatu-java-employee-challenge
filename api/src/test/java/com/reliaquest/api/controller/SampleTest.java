3c354c7f2a9c4227361ce7941f5d6cb96f54e259
yatu360 <yatu360@gmail.com>
2025-04-06 19:08:54 +0100

    Issue 1: added directories, spring boot config, dummy model and config

---
+++ api/src/test/java/com/reliaquest/api/controller/SampleTest.java
@@ -0,0 +1,13 @@
+package com.reliaquest.api.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void sanityCheck() {
+        assertEquals(3, 2 + 1);
+    }
+}
